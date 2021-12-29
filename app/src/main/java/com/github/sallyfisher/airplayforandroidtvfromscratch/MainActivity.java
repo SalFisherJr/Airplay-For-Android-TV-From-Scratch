c357aefa10271ee189d417ff4ec9bfe536790011
sally fisher <sal.fisher.jr@gmail.com>
2021-12-29 15:04:40 -0800

    add wifi info display

--- app/src/main/java/com/github/sallyfisher/airplayforandroidtvfromscratch/MainActivity.java
+++ app/src/main/java/com/github/sallyfisher/airplayforandroidtvfromscratch/MainActivity.java
@@ -2,9 +2,11 @@ package com.github.sallyfisher.airplayforandroidtvfromscratch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 import com.github.sallyfisher.airplayforandroidtvfromscratch.helper.DeviceHelper;
+import com.github.sallyfisher.airplayforandroidtvfromscratch.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayDeviceInfo();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void displayDeviceInfo(){
         TextView textViewDeviceName = (TextView) findViewById(R.id.deviceName);
         textViewDeviceName.setText(DeviceHelper.getDeviceName());
+
+        TextView textViewWifiInfo = (TextView) findViewById(R.id.wifiInfo);
+        if (NetworkUtils.isWifiConnected(this)){
+            textViewWifiInfo.setText("Connection successful!");
+        } else {
+            textViewWifiInfo.setText("Connection failed!");
+        }
     }
 }
 
